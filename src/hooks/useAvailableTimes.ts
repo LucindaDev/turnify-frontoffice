9f5e37b91b7411f14fd827368673714f16f5bb7c
lanzerdev <lanzerdev20@gmail.com>
2025-07-01 19:30:50 -0700

    refactor: Update useAvailableTimes hook parameters to use branch_id instead of opens_at and closes_at

--- src/hooks/useAvailableTimes.ts
+++ src/hooks/useAvailableTimes.ts
@@ -7,12 +7,11 @@ import { Table } from '@/types/branch';
 /**
  * Custom React hook to retrieve available reservation times for a specific branch.
  *
- * @param opens_at - The opening time of the branch in HH:mm format.
- * @param closes_at - The closing time of the branch in HH:mm format.
+ * @param branch_id - The ID of the branch for which to check available reservation slots.
  * @param reservation_date - The date for which to check available reservation slots.
  * @param number_of_guests - The number of guests for the reservation.
  * @returns An array representing the available times for the given parameters.
  */
-export const useAvailableTimes = (opens_at: string, closes_at: string, reservation_date: Date, number_of_guests: number) => {
-    
+export const useAvailableTimes = (branch_id: number, reservation_date: Date, number_of_guests: number) => {
+
 }
